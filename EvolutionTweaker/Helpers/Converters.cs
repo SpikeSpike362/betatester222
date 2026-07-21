c48816f70dd0d731c9a7e209e53e198595e645d7
SpikeSpike362 <spajkspajk362+GitHub@gmail.com>
2026-07-21 15:56:06 +0300

    dccc

--- EvolutionTweaker/Helpers/Converters.cs
+++ EvolutionTweaker/Helpers/Converters.cs
@@ -28,3 +28,20 @@ public class InverseBoolToVisibilityConverter : IValueConverter
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         => throw new NotImplementedException();
 }
+
+/// <summary>
+/// Принимает два значения (элемент списка и выбранную категорию) и возвращает
+/// Visible, если они совпадают, иначе Collapsed. Нужен для подсветки активного пункта меню.
+/// </summary>
+public class SelectedCategoryVisibilityConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values != null && values.Length >= 2 && values[0] != null && Equals(values[0], values[1]))
+            return Visibility.Visible;
+        return Visibility.Collapsed;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
